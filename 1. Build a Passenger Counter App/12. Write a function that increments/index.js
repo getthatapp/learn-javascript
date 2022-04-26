ba4006181b6b115981e117f96e2504dd9f6602f0
Grzegorz Turek <g.turek@yahoo.com>
2022-04-26 20:24:28 +0200

    Lesson 12

--- 1. Build a Passenger Counter App/12. Write a function that increments/index.js
+++ 1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -3,7 +3,13 @@ let lapsCompleted = 0
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
 
+function lapTimer() {
+    lapsCompleted++
+}
 
+lapTimer()
+lapTimer()
+lapTimer()
 
 
 console.log(lapsCompleted)
