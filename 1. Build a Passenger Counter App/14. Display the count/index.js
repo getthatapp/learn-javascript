156ab2cb4619f3a563d0c1890ed5b772b564f526
Grzegorz Turek <g.turek@yahoo.com>
2022-04-26 20:32:55 +0200

    Lesson 14

--- 1. Build a Passenger Counter App/14. Display the count/index.js
+++ 1. Build a Passenger Counter App/14. Display the count/index.js
@@ -1,11 +1,14 @@
 // document.getElementById("count").innerText = 5
 
 // change the count-el in the HTML to reflect the new count
+let countEl = document.getElementById("count-el")
+console.log(countEl)
 
 let count = 0
 
 function increment() {
-    count = count + 1
+    count++
+    countEl.innerText = count
     console.log(count)
 }
 
