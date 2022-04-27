1e24dfd4cfb632b8ade735450cf2b299734b1c6f
Grzegorz Turek <g.turek@yahoo.com>
2022-04-27 20:14:40 +0200

    Lesson 3.6

--- 3. Build a Blackjack game/6. The if...else statement for our game/index.js
+++ 3. Build a Blackjack game/6. The if...else statement for our game/index.js
@@ -1,4 +1,4 @@
-let firstCard = 10
+let firstCard = 15
 let secondCard = 7
 let sum = firstCard + secondCard
 
@@ -8,3 +8,10 @@ let sum = firstCard + secondCard
 // else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
 // else -> "You're out of the game! 😭"
 
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
