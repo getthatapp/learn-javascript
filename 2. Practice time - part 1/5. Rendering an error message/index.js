54fdb6d08585c8520bcb080e2bc5b321cd4d51a2
Grzegorz Turek <g.turek@yahoo.com>
2022-04-27 19:04:06 +0200

    Lesson 2.5

--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -2,4 +2,13 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+let errorPing = document.getElementById("error")
+console.log(errorPing)
+
+function purchase() {
+    console.log("button clicked")     
+    errorPing.textContent = "Something went wrong, please try again"
+}
+
+
 
